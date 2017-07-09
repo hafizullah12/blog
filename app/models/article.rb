331144f8b909c39cf7d91022230d21f71c009eac
hafizullah12 <uhafiz425@gmail.com>
2017-07-09 21:56:18 +0600

    finish blog comments feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,5 @@
 class Article < ApplicationRecord
 
-
-	$a = Article.new
-	$a.title = "This is 1st post"
-
-	$a.body = "I am Masphy From NSTU. I am a RoR Developer.I am Masphy From NSTU. I am a RoR DeveloperI am Masphy From NSTU. I am a RoR Developer"
-
-	$a.save
-	Article.all
+  has_many :comments
+	
 end
